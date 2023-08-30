e9adaea7c4144a6ae0983be2ec70080b08024313
Jelle Maas <typiqally@gmail.com>
2023-08-30 11:32:32 +0200

    Add and configure maven publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import java.net.URI
+
 plugins {
     kotlin("jvm") version "1.9.0"
+    `maven-publish`
 }
 
 group = "com.tpcly"
@@ -16,4 +19,22 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHub"
+            url = URI("https://maven.pkg.github.com/Typiqally/kt-behaviour-tree")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["kotlin"])
+        }
+    }
 }
