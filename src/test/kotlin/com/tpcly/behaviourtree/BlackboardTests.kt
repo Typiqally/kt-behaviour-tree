bdcb03ce20dd4b40d411b65d4f03b96201841482
Jelle Maas <typiqally@gmail.com>
2023-09-12 17:00:19 +0200

    Add blackboard tests

---
+++ src/test/kotlin/com/tpcly/behaviourtree/BlackboardTests.kt
@@ -0,0 +1,42 @@
+package com.tpcly.behaviourtree
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class BlackboardTests {
+    @Test
+    fun testSet() {
+        // Arrange
+        val mapMock = mockk<MutableMap<String, Any>> {
+            every { this@mockk[any()] = any() } answers {}
+        }
+
+        val blackboard = Blackboard(mapMock)
+
+        // Act
+        blackboard["test"] = true
+
+        // Assert
+        verify(exactly = 1) { mapMock["test"] = true }
+    }
+
+    @Test
+    fun testGet() {
+        // Arrange
+        val mapMock = mockk<MutableMap<String, Any>> {
+            every { this@mockk[any()] } returns true
+        }
+
+        val blackboard = Blackboard(mapMock)
+
+        // Act
+        val result = blackboard.get<Boolean>("test")
+
+        // Assert
+        assertEquals(true, result)
+        verify(exactly = 1) { mapMock["test"] }
+    }
+}
