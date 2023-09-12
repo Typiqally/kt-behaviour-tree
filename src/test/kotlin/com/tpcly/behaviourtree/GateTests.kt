f2387c30873b2732578ec49d4664d666b4a65fac
Jelle Maas <typiqally@gmail.com>
2023-09-12 18:01:45 +0200

    Add blackboard based gate tests

--- src/test/kotlin/com/tpcly/behaviourtree/GateTests.kt
+++ src/test/kotlin/com/tpcly/behaviourtree/GateTests.kt
@@ -56,4 +56,52 @@ internal class GateTests {
         assertEquals(Status.FAILURE, result.status)
         verify(exactly = 0) { mockNode.execute(any()) }
     }
+
+    @Test
+    fun testOpenBlackboardGate() {
+        // Arrange
+        val nodeMock = mockk<TreeNode> {
+            every { execute(any()) } returns TreeNodeResult(this, Status.SUCCESS)
+        }
+
+        val blackboardMock = mockk<Blackboard> {
+            every { this@mockk.get<Any>("test") } returns true
+        }
+
+        val node = gate(key = "test", value = true) {
+            nodeMock
+        }
+
+        // Act
+        val result = node.execute(blackboardMock)
+
+        // Assert
+        assertEquals(Status.SUCCESS, result.status)
+        verify(exactly = 1) { blackboardMock.get<Any>("test") }
+        verify(exactly = 1) { nodeMock.execute(any()) }
+    }
+
+    @Test
+    fun testClosedBlackboardGate() {
+        // Arrange
+        val nodeMock = mockk<TreeNode> {
+            every { execute(any()) } returns TreeNodeResult(this, Status.SUCCESS)
+        }
+
+        val blackboardMock = mockk<Blackboard> {
+            every { this@mockk.get<Any>("test") } returns false
+        }
+
+        val node = gate(key = "test", value = true) {
+            nodeMock
+        }
+
+        // Act
+        val result = node.execute(blackboardMock)
+
+        // Assert
+        assertEquals(Status.FAILURE, result.status)
+        verify(exactly = 1) { blackboardMock.get<Any>("test") }
+        verify(exactly = 0) { nodeMock.execute(any()) }
+    }
 }
